9afca0d2df13063747adedf14304ed9aaf3a8970
Zac Tolley <zac@thetolleys.com>
2017-01-23 13:22:33 +0000

    Allow tests to use environment set DB and add circle ci file ready for integration

--- knexfile.js
+++ knexfile.js
@@ -1,7 +1,7 @@
 module.exports = {
   test: {
     client: 'pg',
-    connection: {
+    connection: process.env.DATABASE_URL || {
       host: '127.0.0.1',
       user: 'datahub',
       password: 'password',
