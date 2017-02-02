8acee53608dedf4425cda6f708639bfea129c2d6
Zac Tolley <zac@thetolleys.com>
2017-02-02 22:42:34 +0000

    Add company invest info (#5)
    * Add company investment properties
    
    * Move the schema for company investment summary to own table, makes validation and editing much easier.

--- src/db/companyrepository.js
+++ src/db/companyrepository.js
@@ -8,6 +8,10 @@ function CompanyFamily () {
   return knex('companyfamily')
 }
 
+function CompanyInvestmentSummary () {
+  return knex('companyinvestmentsummary')
+}
+
 function getCompany (companyId) {
   return Companies().where('id', companyId).first()
 }
@@ -40,6 +44,33 @@ function getChildCompanies (id) {
   return CompanyFamily().where('company_parent', id).map(record => record['company_child'])
 }
 
+function getCompanyInvestmentSummary (id) {
+  return CompanyInvestmentSummary().where('id', id).first()
+}
+
+function addCompanyInvestmentSummary (summary) {
+  return CompanyInvestmentSummary()
+    .insert(summary, 'id')
+    .then((result) => {
+      return result[0]
+    })
+}
+
+function updateCompanyInvestmentSummary (summary) {
+  return getCompanyInvestmentSummary(summary.id)
+    .then((result) => {
+      if (!result) {
+        return CompanyInvestmentSummary()
+          .insert(summary, 'id')
+          .then((result) => {
+            return result[0]
+          })
+      } else {
+        return CompanyInvestmentSummary().where('id', summary.id).update(summary)
+      }
+    })
+}
+
 module.exports = {
   getCompany,
   getCompanies,
@@ -47,5 +78,8 @@ module.exports = {
   deleteCompany,
   updateCompany,
   getParentCompanies,
-  getChildCompanies
+  getChildCompanies,
+  getCompanyInvestmentSummary,
+  addCompanyInvestmentSummary,
+  updateCompanyInvestmentSummary
 }
