00433f750abffe8f935b74f99032e3e268e1ae1a
Jim Smith <jim.smith@digital.trade.gov.uk>
2017-02-02 12:18:09 +0000

    Changes to permit easy identification of non-UK companies
    Turned out it was all in the GUIDs

--- src/db/importch.js
+++ src/db/importch.js
@@ -5,6 +5,14 @@ function parseFile (filePath) {
   return csv.data.map(convertCsvToObj)
 }
 
+
+const countryIds = {
+  "united kingdom": "4ed85f99-7e27-4041-ae7f-0440d2b36958",
+    "russia": "6abbee91-7b85-49b8-a133-d59455dc2aad",
+    "united states": "1cb43855-31f9-4cc6-a9a7-893ba5fb0328"
+}
+
+
 function convertCsvToObj (object) {
   const dateParts = object.IncorporationDate.split('/')
 
@@ -18,7 +26,7 @@ function convertCsvToObj (object) {
     registered_address_2: object['RegAddress.AddressLine2'],
     registered_address_3: '',
     registered_address_4: '',
-    registered_address_country: null,
+    registered_address_country: countryIds[object.CountryOfOrigin.toLowerCase()],
     registered_address_county: object['RegAddress.County'],
     registered_address_postcode: object['RegAddress.PostCode'],
     registered_address_town: object['RegAddress.PostTown'],
